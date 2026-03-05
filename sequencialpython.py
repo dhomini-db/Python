d83de6697d0bd8962f92282a60bc442f0045f798
Dhomini De Bom <dbomdhomini@gmail.com>
2026-03-05 20:55:47 -0300

    Add files via upload

---
+++ sequencialpython.py
@@ -0,0 +1,8 @@
+tipo= input("digite o tipo do triangulo:")
+base = int (input("digite o tamanho da base em cm:"))
+altura = int(input("digite a altura em cm:"))
+
+area = base * altura / 2
+print()
+print (f"a area é igual a {area} \n")
+print (f"tipo = {tipo} ")
